242680c2c3b21acb2605fda8a43cfc3926902d29
Youssef <ya103857@gmail.com>
2026-07-15 23:07:35 +0300

    Add files via upload

---
+++ Escape Sequences characters.py
@@ -0,0 +1,33 @@
+
+#  Escape Back space = \b
+print ("Hello\bWorld") # will remove o
+
+print ("Hello Py\bthon") # will remove y
+
+# newline = \
+print ("Hello \
+I love \
+Python")
+
+# Escape Back Slash = \\
+print ("I Love Back Slash \\") 
+
+# Escape Single Quote \'
+print ("I Love Single Quote \'Test\' ")
+
+# Escape Double Quotes = \"
+print ("I Love Double Quotes \"success\" ")
+
+# Line Feed & New Line
+print ("Once Line\nHello World\nSecond Line\nHello Python")
+
+
+# Carriage Return = \r
+print ("123456\rABCDEF")
+
+# Horizontal Tab = \t
+print ("Hello\tPython")
+
+# Character Hex Value = \xhh
+
+print("\x4F\x73")
